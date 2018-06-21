ad978c87c01b44fe391ec04bfe03b7b2c3fa4532
Prashant Chaudhary <101.prashant@gmail.com>
2018-06-21 11:53:19 +0530

    UIDemo

---
+++ UIDemo/src/codekamp/Demo.java
@@ -0,0 +1,70 @@
+package codekamp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by cerebro on 20/06/18.
+ */
+public class Demo implements ActionListener {
+
+    private static JTextField userField;
+    private static JTextField passwordField;
+
+    public static void main(String[] args) {
+        JFrame f1 = new JFrame();
+        f1.setSize(800, 450);
+
+        JPanel p = new JPanel();
+
+
+        JLabel userLabel = new JLabel();
+        userLabel.setText("Username:");
+        userLabel.setForeground(Color.red);
+        p.add(userLabel);
+
+        Demo.userField = new JTextField();
+        Demo.userField.setText("enter username");
+        p.add(Demo.userField);
+
+        JLabel passwordLabel = new JLabel();
+        passwordLabel.setText("Password:");
+        passwordLabel.setForeground(Color.red);
+        p.add(passwordLabel);
+
+        Demo.passwordField = new JTextField();
+        Demo.passwordField.setText("enter password");
+        p.add(Demo.passwordField);
+
+        JButton loginButton = new JButton();
+        loginButton.setText("Login");
+        p.add(loginButton);
+
+        Demo d1 = new Demo();
+        loginButton.addActionListener(d1);
+
+        f1.add(p);
+
+        f1.setVisible(true);
+    }
+
+    public static void login(String username, String password) {
+        System.out.println("username entered is " + username);
+        System.out.println("password entered is " + password);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String username = Demo.userField.getText();
+        String password = Demo.passwordField.getText();
+        Demo.login(username, password);
+    }
+
+    // Any JButton always calls actionPerformed(ActionEvent e) method when it is clicked
+    // So we can't control which method will be called when button is clicked
+    // But what we can control is the object on which actionPerformed method
+    // ...will be called.
+
+
+}
